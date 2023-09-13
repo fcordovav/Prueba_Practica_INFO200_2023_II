783ed17cca4e183f70b8fd63c232d40627c4685b
PipeCordova <felipe.cordova@alumnos.uach.cl>
2023-09-13 16:23:16 -0300

    main

--- scripts/holaMundo.py
+++ scripts/holaMundo.py
@@ -1,32 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Genera algunos datos de ejemplo
-x = np.linspace(0, 10, 100)
-y = np.sin(x)
+def main() -> None:
+    # Genera algunos datos de ejemplo
+    x = np.linspace(0, 10, 100)
+    y = np.sin(x)
 
-# Crea el gráfico
-plt.figure(figsize=(8, 6))
-plt.plot(x, y, label='Función seno')
-plt.title('Gráfico de la función seno')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.legend()
+    # Crea el gráfico
+    plt.figure(figsize=(8, 6))
+    plt.plot(x, y, label='Función seno')
+    plt.title('Gráfico de la función seno')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend()
 
-# Guarda la imagen en una carpeta aparte
-carpeta_salida = 'Prueba_Practica_INFO200/carpeta imagenes'
-nombre_archivo = 'grafico_seno.png'
-ruta_completa = f'{carpeta_salida}/{nombre_archivo}'
+    # Guarda la imagen en una carpeta aparte
+    carpeta_salida = 'Prueba_Practica_INFO200/carpeta imagenes'
+    nombre_archivo = 'grafico_seno.png'
+    ruta_completa = f'{carpeta_salida}/{nombre_archivo}'
 
-# Asegúrate de que la carpeta de salida exista
-import os
-os.makedirs(carpeta_salida, exist_ok=True)
+    # Asegúrate de que la carpeta de salida exista
+    import os
+    os.makedirs(carpeta_salida, exist_ok=True)
 
-# Guarda la imagen en formato PNG
-plt.savefig(ruta_completa, dpi=300, bbox_inches='tight')
+    # Guarda la imagen en formato PNG
+    plt.savefig(ruta_completa, dpi=300, bbox_inches='tight')
 
-# Muestra el gráfico en pantalla (opcional)
-plt.show()
+    # Muestra el gráfico en pantalla (opcional)
+    plt.show()
 
-print(f'La imagen se ha guardado en: {ruta_completa}')
+    print(f'La imagen se ha guardado en: {ruta_completa}')
 
+main()
