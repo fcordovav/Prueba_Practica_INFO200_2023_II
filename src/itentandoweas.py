529833d6d9567f5b8f0314d06bf7d3111f8c0739
Renato <renatoatencioaedo@gmail.com>
2023-09-30 14:18:16 -0300

    Para graficar las restricciones y zona factible=puntos

--- src/itentandoweas.py
+++ src/itentandoweas.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 from shapely.geometry import Polygon
 import sympy as sp
 
+#########################################
+#### Para graficas las restricciones ####
+#########################################
 
 ### Formato [Cant1X, Cant2Y, LadoDerecho] ###
 restriccionesMenorIgual = [
@@ -10,7 +13,6 @@ restriccionesMenorIgual = [
     [30, 10, 15000],
     [0, 1, 700],
 ]
-
 restriccionesMayorIgual = [
     [1,0,100],
     [0,1,100]
@@ -30,7 +32,7 @@ def encontrar_interseccion(restriccion1, restriccion2):
     except np.linalg.LinAlgError:
         return None
 
-# Iterar a través de todas las combinaciones de restricciones
+# Iterar a través de todas las combinaciones de restricciones, Para encontrar las intersecciones
 for i, restriccion1 in enumerate(restriccionesMenorIgual + restriccionesMayorIgual):
     for j, restriccion2 in enumerate(restriccionesMenorIgual + restriccionesMayorIgual):
         if i != j:  # Evitar comparar la misma restricción
@@ -38,18 +40,9 @@ for i, restriccion1 in enumerate(restriccionesMenorIgual + restriccionesMayorIgu
             if punto is not None:
                 puntos_interseccion.append(punto)
 
-# # Mostrar los puntos de intersección
-# if len(puntos_interseccion) > 0:
-#     print("Puntos de intersección:")
-#     for i, punto in enumerate(puntos_interseccion):
-#         print(f"Punto {i + 1}: ({punto[0]}, {punto[1]})")
-# else:
-#     print("No se encontraron puntos de intersección válidos.")
-
-### Ver que puntos cumplen todas las restricciones ###
+### Para ver que puntos de puntos_interseccion cumplen todas las condiciones ###
 
 puntosPolinomio = []
-
 # Verificar si los puntos de intersección cumplen todas las restricciones
 for punto in puntos_interseccion:
     cumple_todas = True
@@ -64,72 +57,60 @@ for punto in puntos_interseccion:
     if cumple_todas:
         puntosPolinomio.append(punto)
 
-# # Mostrar los puntos que cumplen todas las restricciones
-# if len(puntosPolinomio) > 0:
-#     print("Puntos que cumplen todas las restricciones:")
-#     for i, punto in enumerate(puntosPolinomio):
-#         print(f"Punto {i + 1}: ({punto[0]}, {punto[1]})")
-# else:
-#     print("No se encontraron puntos que cumplan todas las restricciones.")
-
-### Eliminar puntos repetidos ###
+### Eliminar puntos repetidos en puntosPolinomio ###
 
 puntosPolinomio = list(set(tuple(p) for p in puntosPolinomio))
 
-# # Mostrar los puntos únicos que cumplen todas las restricciones
-# if len(puntosPolinomio) > 0:
-#     print("Puntos únicos que cumplen todas las restricciones:")
-#     for i, punto in enumerate(puntosPolinomio):
-#         print(f"Punto {i + 1}: ({punto[0]}, {punto[1]})")
-# else:
-#     print("No se encontraron puntos únicos que cumplan todas las restricciones.")
-
-# print(puntosPolinomio)
-
-# ### Graficar las restricciones ###
-
-# X = np.linspace(0, 2000, 400)  # Valores de X de 0 a 2000
-# Y = np.linspace(0, 2000, 400)  # Valores de Y de 0 a 2000
-
-# # Crear una cuadrícula de valores X e Y
-# X, Y = np.meshgrid(X, Y)
-
-# # Calcular las desigualdades para las restricciones MenorIgual
-# for restriccion in restriccionesMenorIgual:
-#     Z = restriccion[0] * X + restriccion[1] * Y - restriccion[2]
-#     plt.contour(X, Y, Z, levels=[0], colors='r')
-
-# # Calcular las desigualdades para las restricciones MayorIgual
-# for restriccion in restriccionesMayorIgual:
-#     Z = restriccion[0] * X + restriccion[1] * Y - restriccion[2]
-#     plt.contour(X, Y, Z, levels=[0], colors='b')
-
-# # Configurar los ejes y mostrar el gráfico
-# plt.xlabel('X')
-# plt.ylabel('Y')
-# plt.xlim(0, 2000)
-# plt.ylim(0, 2000)
-# plt.grid(True)
-# plt.show()
-
-### Graficar zona factible ###
-
-puntos = np.array(puntosPolinomio)
-
-# Separar las coordenadas X e Y
-x_coords = puntos[:, 0]
-y_coords = puntos[:, 1]
-
-# Crear un polígono cerrado conectando los puntos
-plt.fill(x_coords, y_coords, 'b', alpha=0.3)  # 'b' es para color azul, alpha ajusta la transparencia
-
-# Agregar puntos individuales en el gráfico
-plt.scatter(x_coords, y_coords, color='red', marker='o', label='Puntos')
-
-# Configurar ejes y mostrar el gráfico
+### Graficar las restricciones como igualdades ###
+X = np.linspace(0, 2000, 400)  # Valores de X de 0 a 2000
+Y = np.linspace(0, 2000, 400)  # Valores de Y de 0 a 2000
+# Crear una cuadrícula de valores X e Y
+X, Y = np.meshgrid(X, Y)
+# Calcular las desigualdades para las restricciones MenorIgual
+for restriccion in restriccionesMenorIgual:
+    Z = restriccion[0] * X + restriccion[1] * Y - restriccion[2]
+    plt.contour(X, Y, Z, levels=[0], colors='r')
+# Calcular las desigualdades para las restricciones MayorIgual
+for restriccion in restriccionesMayorIgual:
+    Z = restriccion[0] * X + restriccion[1] * Y - restriccion[2]
+    plt.contour(X, Y, Z, levels=[0], colors='b')
+# Configurar los ejes y mostrar el gráfico
 plt.xlabel('X')
 plt.ylabel('Y')
-plt.title('Área del polígono')
+plt.xlim(0, 2000)
+plt.ylim(0, 2000)
 plt.grid(True)
+plt.show()
+
+#####################################
+#### Para Graficar zona factible ####
+#####################################
+
+# Definir el rango de valores de x y y
+x = np.linspace(0, 2000, 400)  # Rango de valores de x
+y = np.linspace(0, 2000, 400)  # Rango de valores de y
+# Crear una cuadrícula de puntos (x, y)
+x, y = np.meshgrid(x, y)
+# Definir las restricciones
+condicion1 = 20*x + 100*y <= 73000
+condicion2 = 30*x + 10*y <= 15000
+condicion3 = y <= 700
+condicion4 = x >= 100
+condicion5 = y >= 100
+# Combinar las restricciones con AND
+region_de_interes = condicion1 & condicion2 & condicion3 & condicion4 & condicion5
+# Crear una máscara booleana para la región que cumple con las restricciones
+region_de_interes = np.ma.masked_where(region_de_interes == False, region_de_interes)
+# Graficar la región de interés
+plt.figure(figsize=(8, 6))
+plt.contourf(x, y, region_de_interes, cmap="viridis", alpha=0.5)
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.title("Región que cumple con las restricciones")
+plt.grid(True)
+# Para mostrar los puntos en la region factible
+x_puntos, y_puntos = zip(*puntosPolinomio)  # Separar las coordenadas x e y de la lista
+plt.scatter(x_puntos, y_puntos, c='red', label='Puntos', marker='o')
 plt.legend()
+plt.grid(True)
 plt.show()
